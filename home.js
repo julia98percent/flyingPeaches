213276181993ed7caec5c3690751fbb389f0873a
julia98percent <ji_0315@naver.com>
2021-07-17 15:22:50 +0900

    v contents align

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 function slideShow() {
   var i;
-  var x = document.getElementsByClassName("item"); //slide1에 대한 dom 참조
+  var x = document.getElementsByClassName("slide-item"); //slide1에 대한 dom 참조
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none"; //처음에 전부 display를 none으로 한다.
   }
@@ -9,5 +9,5 @@ function slideShow() {
     index = 1; //인덱스가 초과되면 1로 변경
   }
   x[index - 1].style.display = "block"; //해당 인덱스는 block으로
-  setTimeout(slideShow, 100000);
+  setTimeout(slideShow, 10000000);
 }
